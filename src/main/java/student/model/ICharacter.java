0513cf1ee3cfad1085a6ba42bc32a5478c11d2f1
xiwei22 <lixiwei22@gmail.com>
2024-07-27 11:12:11 -0400

    modify ICharacter and Character

--- src/main/java/student/model/ICharacter.java
+++ src/main/java/student/model/ICharacter.java
@@ -6,29 +6,31 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.util.List;
 
-/** Interface to the model. **/
+/**
+ * Interface to the model.
+ **/
 
 public interface ICharacter {
 
-    void loadCharacters(String name, String status, String species, String gender, List<String> episodes);
+    void loadCharacters(String name, String status, String species, String gender, List<String> episodes, boolean ascending);
 
     /**
      * Character record to pass around between objects.
      * Uses Jackson annotations to map JSON fields to Java fields.
      *
-     * @param id   The character's unique identifier. User will not see this.
-     *             // not sure if we need to include this in the record
-     * @param name The character's name.
-     * @param status The character's status (alive, dead, unknown).
+     * @param id      The character's unique identifier. User will not see this.
+     *                // not sure if we need to include this in the record
+     * @param name    The character's name.
+     * @param status  The character's status (alive, dead, unknown).
      * @param species The character's species.
-     * @param gender The character's gender.
-     * @param image The character's image URL.
+     * @param gender  The character's gender.
+     * @param image   The character's image URL.
      * @param episode The character's episode URLs.
      */
     @JsonIgnoreProperties(ignoreUnknown = true)
-    @JacksonXmlRootElement(localName = "characterList")
-    @JsonPropertyOrder({"id", "name", "status", "species","gender","image","episode"})
+    @JacksonXmlRootElement(localName = "results")
+    @JsonPropertyOrder({"id", "name", "status", "species", "gender", "image", "episode"})
     record CharacterRecord(int id, String name, String status, String species, String gender,
-                     String image, String[] episode) {
+                           String image, String[] episode) {
     }
 }
