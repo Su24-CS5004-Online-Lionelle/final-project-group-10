941d177602fd647f55184146fb505f93ffcb35d3
JerryYanTY <yan.tianyu@northeastern.edu>
2024-08-02 15:10:59 -0400

    Added some skeleton codes

--- src/main/java/student/view/JFrameView.java
+++ src/main/java/student/view/JFrameView.java
@@ -1,5 +1,22 @@
 package student.view;
+import javax.swing.*;
+
+
+import student.controller.CharacterController;
 
 // feel free to rename
-public class JFrameView {
+public class JFrameView extends JFrame{
+
+    private CharacterController controller;
+
+    public JFrameView(CharacterController controller){
+        setTitle("CharacterApp");
+        this.controller = controller;
+
+    }
+
+    public void start() {
+
+    }
+
 }
