836fbe8760d157c20f06238ad679acd99a129717
JerryYanTY <yan.tianyu@northeastern.edu>
2024-08-02 15:15:34 -0400

    Update Settings.java

--- src/main/java/student/Settings.java
+++ src/main/java/student/Settings.java
@@ -1,5 +1,13 @@
 package student;
 
 public class Settings {
-    
+    private static Settings instance;
+    public final String SEARCH;
+    public final String GENDER;
+    public final String STATUS;
+    public final String SPECIES;
+    public final String EXPORT;
+    public final String SORT;
+    public final String FONT
+    public final int FONT_SIZE;
 }
