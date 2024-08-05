278446dc6e78e1a45c16263830bae754763f6ae3
gsjc97 <gracesjchong@gmail.com>
2024-08-04 21:17:25 -0400

    add: write, load, getter methods from model layer to controller

--- src/main/java/student/controller/CharacterController.java
+++ src/main/java/student/controller/CharacterController.java
@@ -1,11 +1,11 @@
 package student.controller;
 
-import student.model.Character;
 import student.model.ICharacter;
 import student.model.formatters.DataFormatter;
 import student.model.formatters.Formats;
 import student.view.JFrameView;
 
+import java.io.OutputStream;
 import java.util.List;
 
 // feel free to rename
@@ -26,5 +26,16 @@ public class CharacterController {
         this.characters = character;
     }
 
+    public void writeCharacters(List<ICharacter.CharacterRecord> characters, int format_choice, OutputStream output) {
+        Formats format = Formats.values()[format_choice];
+        DataFormatter.write(characters, format, output);
+    }
 
+    public void loadCharacters(String name, String status, String species, String gender, boolean ascending) {
+        characters.loadCharacters(name, status, species, gender, ascending);
+    }
+
+    public List<ICharacter.CharacterRecord> getCharacterRecords() {
+        return characters.getCharacterRecords();
+    }
 }
