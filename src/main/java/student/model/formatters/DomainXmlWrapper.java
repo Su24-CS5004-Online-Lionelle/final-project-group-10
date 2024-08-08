62568c03aa711db403a5d475effaf36b0a3a27ce
gsjc97 <gracesjchong@gmail.com>
2024-08-08 17:18:58 -0400

    fix: javadoc comments model util classes

---
+++ src/main/java/student/model/formatters/DomainXmlWrapper.java
@@ -0,0 +1,26 @@
+package student.model.formatters;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import student.model.ICharacter.CharacterRecord;
+import java.util.Collection;
+
+/**
+ * Wrapper class for XML serialization.
+ */
+@JacksonXmlRootElement(localName = "characterList")
+public final class DomainXmlWrapper {
+
+    /** List of the records. */
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private Collection<CharacterRecord> domain;
+
+    /**
+     * Constructor for the DomainXmlWrapper.
+     *
+     * @param characters The list of character records to wrap.
+     */
+    public DomainXmlWrapper(Collection<CharacterRecord> characters) {
+        this.domain = characters;
+    }
+}
