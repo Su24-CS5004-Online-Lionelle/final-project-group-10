a248bd06d970761555d8e6d2d8a68edba208a543
gsjc97 <gracesjchong@gmail.com>
2024-08-08 17:05:31 -0400

    fix: checkstyle and javadoc for Button class

--- src/main/java/student/view/Button.java
+++ src/main/java/student/view/Button.java
@@ -22,56 +22,42 @@ public class Button extends JButton {
      * Enum for the type of button.
      */
     enum ButtonType {
-        /**
-         * Search button.
-         */
+        /** Search button. */
         SEARCH,
-        /**
-         * Export button.
-         */
+        /** Export button. */
         EXPORT,
-        /**
-         * Next button.
-         */
+        /** Next button. */
         NEXT,
-        /**
-         * Previous button.
-         */
+        /** Previous button. */
         PREVIOUS
     }
 
-    /**
-     * Controller for the program.
-     */
+    /** Controller for the program. */
     private final CharacterController controller;
-    /**
-     * Type of button.
-     */
+    /** Type of button. */
     private final ButtonType bt;
-    /**
-     * Text field for search.
-     */
+    /** Text field for search. */
     private final JTextField searchField;
-    /**
-     * Dropdown for gender.
-     */
+    /** Dropdown menu to filter by gender. */
     private final JComboBox<String> genderBox;
+    /** Dropdown menu to filter by status. */
     private final JComboBox<String> statusBox;
+    /** Dropdown menu to filter by species. */
     private final JComboBox<String> speciesBox;
+    /** Dropdown menu to sort results by name. */
     private final JComboBox<String> sortBox;
 
-
-
     /**
-     * @param buttonType
-     * @param controller
-     * @param searchField
-     * @param genderBox
-     * @param statusBox
-     * @param speciesBox
-     * @param sortBox
+     * Constructor for the Buttons in the JFrame. 
+     * 
+     * @param buttonType button types include search, export, previous, and next buttons.
+     * @param controller the controller for the program.
+     * @param searchField the text field to search by name.
+     * @param genderBox the dropdown menu to flter by gender.
+     * @param statusBox the dropdown menu to filter by status.
+     * @param speciesBox the dropdown menu to filter by species.
+     * @param sortBox the dropdown menu to sort results by name. 
      */
-
     public Button(ButtonType buttonType, CharacterController controller, JTextField searchField,
                                     JComboBox<String> genderBox, JComboBox<String> statusBox,
                                     JComboBox<String> speciesBox, JComboBox<String> sortBox) {
@@ -85,7 +71,14 @@ public class Button extends JButton {
         addActionListener(new ButtonListener());
     }
 
-
+    /**
+     * Creates the action listeners for each button type,
+     * search, export, next, and previous.
+     * The search button will load the characters and display them.
+     * The export button will save the characters to a file.
+     * The next button will display the next page of characters, if there is one.
+     * The previous button will display the previous page of characters, if there is one.
+     */
     private class ButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             switch (bt) {
@@ -95,25 +88,26 @@ public class Button extends JButton {
                     break;
 
                 case EXPORT:
-                    String name1 = searchField.getText();
-                    String gender1 = (String) genderBox.getSelectedItem();
-                    String status1 = (String) statusBox.getSelectedItem();
-                    String species1 = (String) speciesBox.getSelectedItem();
-                    String sort1 = (String) sortBox.getSelectedItem();
+                    String name = searchField.getText();
+                    String gender = (String) genderBox.getSelectedItem();
+                    String status = (String) statusBox.getSelectedItem();
+                    String species = (String) speciesBox.getSelectedItem();
+                    String sort = (String) sortBox.getSelectedItem();
 
-                    if ("all".equalsIgnoreCase(gender1)) {
-                        gender1 = "";
+                    if ("all".equalsIgnoreCase(gender)) {
+                        gender = "";
                     }
-                    if ("all".equalsIgnoreCase(status1)) {
-                        status1 = "";
+                    if ("all".equalsIgnoreCase(status)) {
+                        status = "";
                     }
-                    if ("all".equalsIgnoreCase(species1)) {
-                        species1 = "";
+                    if ("all".equalsIgnoreCase(species)) {
+                        species = "";
                     }
 
-                    boolean ascending1 = "ascending".equalsIgnoreCase(sort1);
-                    List<ICharacter.CharacterRecord> characters1 = 
-                            controller.loadCharacters(name1, status1, species1, gender1, ascending1);
+                    boolean ascending = "ascending".equalsIgnoreCase(sort);
+
+                    List<ICharacter.CharacterRecord> characters = 
+                            controller.loadCharacters(name, status, species, gender, ascending);
                     JFileChooser fileChooser = new JFileChooser();
                     fileChooser.setDialogTitle("Save File");
 
@@ -167,6 +161,11 @@ public class Button extends JButton {
         }
     }
 
+    /**
+     * Loads the characters based on the search criteria.
+     * If the option selected for gender, status, and/or species is "all",
+     * then no filter is applied to these fields.
+     */
     private void loadCharacters() {
         String name = searchField.getText();
         String gender = (String) genderBox.getSelectedItem();
@@ -189,6 +188,11 @@ public class Button extends JButton {
         controller.loadURL(name, status, species, gender, ascending);
     }
 
+    /** 
+     * Displays the characters based on the search criteria.
+     * If there is a next page, the next button in the JFrame will be enabled.
+     * If there is a previous page, the previous button in the JFrame will be enabled.
+     */
     private void displayChar() {
         String sort = (String) sortBox.getSelectedItem();
         boolean ascending = "ascending".equalsIgnoreCase(sort);
