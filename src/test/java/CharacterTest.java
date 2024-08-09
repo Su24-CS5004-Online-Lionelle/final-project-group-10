7a6db65c1ee26f035e8fe6e2875bd3e4001b43be
gsjc97 <gracesjchong@gmail.com>
2024-08-09 09:55:57 -0400

    add: tests for getter and setter methods in Character class

--- src/test/java/CharacterTest.java
+++ src/test/java/CharacterTest.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import student.model.Character;
 import student.model.ICharacter;
 import student.model.formatters.DataFormatter;
@@ -16,6 +21,7 @@ import java.util.List;
  */
 public class CharacterTest {
 
+    /** The Character model. */
     private Character character = null;
 
     /**
@@ -32,26 +38,21 @@ public class CharacterTest {
      */
     @Test
     public void testLoadCharactersBasicInfo() {
-        character.loadCharacters("111", "Alive", "Human", "Male", false);
+        character.loadCharacters("Rick Sanchez", "Alive", "Human", "Male", false);
         List<ICharacter.CharacterRecord> characterRecords = character.getCharacterRecords();
 
-        //view.updateTable(characterRecords)
-        System.out.println(1);
+        assertEquals(2, characterRecords.size());
     }
 
     /**
-     * Tests the loading of character records with a filter on episodes.
+     * Tests the loading of character records with no filters.
      * Ensures that the loadCharacters method can handle null values for some filters correctly.
      */
     @Test
-    public void testLoadCharactersWithEpisodeFilter() {
-//        character.loadCharacters("Rick Sanchez", null, null, null, null);
+    public void testLoadCharactersWithNullFilters() {
         character.loadCharacters("Rick Sanchez", null, null, null, false);
         List<ICharacter.CharacterRecord> characterRecords = character.getCharacterRecords();
-
-        //view.updateTable(characterRecords)
-
-        System.out.println(1);
+        assertEquals(4, characterRecords.size());
     }
 
     /**
@@ -68,7 +69,8 @@ public class CharacterTest {
 
             //VIEW  need to select a path for the file
             //new File("C:\\Users\\56555\\Desktop\\data." + suffix)
-            DataFormatter.write(characterRecords, format, new FileOutputStream(new File("C:\\Users\\56555\\Desktop\\data." + suffix)));
+            DataFormatter.write(characterRecords, format,
+            new FileOutputStream(new File("C:\\Users\\56555\\Desktop\\data." + suffix)));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -79,11 +81,74 @@ public class CharacterTest {
      */
     @Test
     public void testLoadCharactersWithMultiplePages() {
-        character.loadCharacters("Rick Sanchez", null, null, null, false);
+        character.loadCharacters("Rick", null, null, null, false);
         List<ICharacter.CharacterRecord> characterRecords = character.getCharacterRecords();
+        assertEquals(107, characterRecords.size());
+    }
 
-        //view.updateTable(characterRecords)
+    /**
+     * Tests loading of characters with no results.
+     */
+    @Test
+    public void testLoadCharactersInvalidSearch() {
+        character.loadCharacters("Invalid Search", null, null, null, false);
+        List<ICharacter.CharacterRecord> characterRecords = character.getCharacterRecords();
+        assertEquals(0, characterRecords.size());
+    }
 
-        System.out.println(1);
+    /** Test getURL method. */
+    @Test
+    void testGetURL() {
+        character.loadURL("Rick", "Alive", "Human", "Male", true);
+        assertNotNull(character.getURL(0));
+        assertNull(character.getURL(-1));
+        assertNull(character.getURL(character.getCurrIndex() + 1));
+    }
+
+    /** Test loadURL method. */
+    @Test
+    void testLoadURL() {
+        character.loadURL("Rick", "Alive", "Human", "Male", true);
+        assertNotNull(character.getURL(0));
+        assertNull(character.getURL(-1));
+    }
+
+    /** Test getCharByPage method. */
+    @Test
+    void testGetCharByPage() {
+        character.loadURL("Rick", "Alive", "Human", "Male", true);
+        List<ICharacter.CharacterRecord> characters = character.getCharByPage(true);
+        assertNotNull(characters);
+        assertFalse(characters.isEmpty());
+        assertTrue(characters.get(0).name().contains("Rick"));
+    }
+
+    /** Test getter method getCurrIndex. */
+    @Test
+    void testGetCurrIndex() {
+        assertEquals(0, character.getCurrIndex());
+    }
+
+    /** Test setter method setCurrIndex. */
+    @Test
+    void testSetCurrIndex() {
+        character.setCurrIndex(5);
+        assertEquals(5, character.getCurrIndex());
+    }
+
+    /** Test increasePages method. */
+    @Test
+    void testIncreasePages() {
+        character.setCurrIndex(2);
+        character.increasePages();
+        assertEquals(3, character.getCurrIndex());
+    }
+
+    /** Test decreasePages method. */
+    @Test
+    void testDecreasePages() {
+        character.setCurrIndex(2);
+        character.decreasePages();
+        assertEquals(1, character.getCurrIndex());
     }
 }
