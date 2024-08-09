001b8fa245438395f2fd4d8e7a27e5c79aff9b4b
Candice <155860581+Lxsong77@users.noreply.github.com>
2024-08-08 21:49:37 -0400

    Update Menu.java

--- src/main/java/student/view/Menu.java
+++ src/main/java/student/view/Menu.java
@@ -6,14 +6,27 @@ import javax.swing.*;
 import java.awt.event.*;
 
 
+/**
+ * This class represents the main menu interface for the application,
+ * allowing users to select various options such as gender, status, species, and sort order.
+ * This class is responsible for setting up the UI components and handling item selection events.
+ */
 public class Menu extends JFrame implements ItemListener {
+    /**An instance of the application settings.*/
     private static final Settings SETTINGS = Settings.getInstance();
+    /**ComboBox for selecting gender options.*/
     static JComboBox<String> gender_box;
+     /**ComboBox for selecting status options.*/
     static JComboBox<String> status_box;
+    /**ComboBox for selecting species options.*/
     static JComboBox<String> species_box;
+    /**ComboBox for selecting sort options.*/
     static JComboBox<String> sort_box;
 
-
+    /**
+     * Constructs a new `Menu` object, initializing the UI components based on
+     * the settings retrieved from the `Settings` singleton.
+     */
     public Menu() {
         String[] genders = SETTINGS.getGenderOption().split(",");
         String[] statuses = SETTINGS.getStatusOption().split(",");
@@ -44,18 +57,38 @@ public class Menu extends JFrame implements ItemListener {
 
     }
 
+    /**
+     * Returns the gender selection combo box.
+     *
+     * @return the combo box for gender selection
+     */
     public JComboBox<String> getGenderBox() {
         return gender_box;
     }
 
+    /**
+     * Returns the status selection combo box.
+     *
+     * @return the combo box for status selection
+     */
     public JComboBox<String> getStatusBox() {
         return status_box;
     }
 
+    /**
+     * Returns the species selection combo box.
+     *
+     * @return the combo box for species selection
+     */
     public JComboBox<String> getSpeciesBox() {
         return species_box;
     }
 
+    /**
+     * Returns the sort selection combo box.
+     *
+     * @return the combo box for sort selection
+     */
     public JComboBox<String> getSortBox() {
         return sort_box;
     }
